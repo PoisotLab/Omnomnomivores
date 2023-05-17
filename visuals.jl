c39810be9fbaa2f7c1a4bd7b5bde26ab727d3ed4
Tanya Strydom <tanya.strydom@icloud.com>
2023-05-17 11:59:22 +0200

    🏖️ sandbox

--- visuals.jl
+++ visuals.jl
@@ -3,10 +3,13 @@ using Plots
 heatmap(environment_value)
 
 heatmap(meta_comm[:, :, 1, 100])
-heatmap(meta_comm[:, :, 5, 1])
+heatmap(meta_comm[:, :, 6, 1])
 heatmap(meta_comm[:, :, 7, 1])
 
 anim = @animate for i=1:100
-    heatmap(meta_comm[:, :, 1, i])
+   plot(heatmap(meta_comm[:, :, 2, i], title="Plant"),
+   heatmap(meta_comm[:, :, 6, i], title="Herbivore"),
+   heatmap(meta_comm[:, :, 7, i], title="Carnivore"),
+   layout=(3,1))
 end
-gif(anim, "figures/herbivore_test.gif", fps = 15)
+gif(anim, "figures/all_test.gif", fps = 1)
