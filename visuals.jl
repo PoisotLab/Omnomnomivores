8486c49ab8141ce318d15058aa9a2d205d200c7f
Tanya Strydom <tanya.strydom@icloud.com>
2023-05-17 11:48:07 +0200

    :bug: we have some figures! but also a time related :bug:

--- visuals.jl
+++ visuals.jl
@@ -1,1 +1,12 @@
 using Plots
+
+heatmap(environment_value)
+
+heatmap(meta_comm[:, :, 1, 100])
+heatmap(meta_comm[:, :, 5, 1])
+heatmap(meta_comm[:, :, 7, 1])
+
+anim = @animate for i=1:100
+    heatmap(meta_comm[:, :, 1, i])
+end
+gif(anim, "figures/herbivire_test.gif", fps = 15)
