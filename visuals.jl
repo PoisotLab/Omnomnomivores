06fb67a9a7db32a8230570c22bd99bb27226da96
Tanya Strydom <tanya.strydom@icloud.com>
2023-05-17 15:01:57 +0200

    :bug: doesn't fix #1 but at least we have figures?

--- visuals.jl
+++ visuals.jl
@@ -1,15 +1,20 @@
+using CairoMakie
+using GLMakie
+using Makie.Colors
 using Plots
 
-heatmap(environment_value)
-
-heatmap(meta_comm[:, :, 1, 100])
-heatmap(meta_comm[:, :, 6, 1])
-heatmap(meta_comm[:, :, 7, 1])
+# get global extrema
+extremas = map(extrema, meta_comm)
+global_min = minimum(t->first(t), extremas)
+global_max = maximum(t->last(t), extremas)
+# these limits have to be shared by the maps and the colorbar
+clims = (global_min, global_max)
 
 anim = @animate for i=1:100
-   plot(heatmap(meta_comm[:, :, 2, i], title="Plant"),
-   heatmap(meta_comm[:, :, 6, i], title="Herbivore"),
-   heatmap(meta_comm[:, :, 7, i], title="Carnivore"),
-   layout=(3,1))
+   plot(heatmap(environment_value, title="Environment"),
+   heatmap(meta_comm[:, :, 1, i], title="Plant (Eopt 0.25)", clims = (global_min, global_max)),
+   heatmap(meta_comm[:, :, 6, i], title="Herbivore (Eopt 1.0)", clims = (global_min, global_max)),
+   heatmap(meta_comm[:, :, 7, i], title="Carnivore (Eopt 0.5)", clims = (global_min, global_max)),
+   layout=(2,2))
 end
 gif(anim, "figures/all_test.gif", fps = 1)
