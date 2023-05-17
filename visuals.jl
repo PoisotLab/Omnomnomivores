e3d8768849e6aa7ab70424a06d0cae713343fb71
Tanya Strydom <tanya.strydom@icloud.com>
2023-05-17 11:48:33 +0200

    🙃 save then commit....

--- visuals.jl
+++ visuals.jl
@@ -9,4 +9,4 @@ heatmap(meta_comm[:, :, 7, 1])
 anim = @animate for i=1:100
     heatmap(meta_comm[:, :, 1, i])
 end
-gif(anim, "figures/herbivire_test.gif", fps = 15)
+gif(anim, "figures/herbivore_test.gif", fps = 15)
