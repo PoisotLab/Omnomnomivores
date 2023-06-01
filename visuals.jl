00709898fe5c1a4056d14ad996a847b57c5ef7b4
Tanya Strydom <tanya.strydom@icloud.com>
2023-06-01 14:43:37 +0200

    :up: update code to generate gifs

--- visuals.jl
+++ visuals.jl
@@ -4,17 +4,17 @@ using Makie.Colors
 using Plots
 
 # get global extrema
-extremas = map(extrema, meta_comm)
+extremas = map(extrema, metacommunity)
 global_min = minimum(t->first(t), extremas)
 global_max = maximum(t->last(t), extremas)
 # these limits have to be shared by the maps and the colorbar
 clims = (global_min, global_max)
 
-anim = @animate for i=1:100
-   plot(heatmap(environment_value, title="Environment"),
-   heatmap(meta_comm[:, :, 1, i], title="Plant (Eopt 0.25)"),
-   heatmap(meta_comm[:, :, 6, i], title="Herbivore (Eopt 1.0)"),
-   heatmap(meta_comm[:, :, 7, i], title="Carnivore (Eopt 0.5)"),
+anim = @animate for i=1:generations
+   Plots.plot(Plots.heatmap(environment, title="Environment"),
+   Plots.heatmap(metacommunity[:, :, 1, i], title="Plant", clims = clims),
+   Plots.heatmap(metacommunity[:, :, 55, i], title="Herbivore", clims = clims),
+   Plots.heatmap(metacommunity[:, :, 78, i], title="Carnivore", clims = clims),
    layout=(2,2))
 end
 gif(anim, "figures/all_test.gif", fps = 1)
