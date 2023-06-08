46ee8b3a12e3d160364aa18d4c6c079f23657387
Tanya Strydom <tanya.strydom@icloud.com>
2023-06-08 10:47:33 +0200

    📈 gradual invasion

--- 01_code.jl
+++ 01_code.jl
@@ -6,7 +6,7 @@ using ProgressMeter
 
 landscape_size = (20, 20)
 species_richness = 80
-generations = 150
+generations = 200
 
 # Load the functions we need from the lib folder
 include("lib/01_species_creation.jl")
@@ -30,7 +30,9 @@ set_dispersal_decay!(dispersal_decay; trophic_level)
 
 # Set an initial metacommunity object, only the first timestep for plants is set to 10.0
 metacommunity = fill(0.0, (landscape_size..., species_richness, generations))
-metacommunity[:, :, findall(trophic_level .< 2), 1] .= 10.0
+metacommunity[:, :, findall(trophic_level .< 2), 1] .= 100.0
+metacommunity[:, :, findall(trophic_level .== 2), 50] .= 50.0
+metacommunity[:, :, findall(trophic_level .== 3), 100] .= 10.0
 
 # Run the model
 simulate!(
@@ -41,24 +43,3 @@ simulate!(
     environmental_optimum,
     interaction_strength,
 )
-
-# Some diagnostic plots
-using GLMakie
-fig = Figure()
-axs = [
-    Axis(fig[1, 1]),
-    Axis(fig[1, 2]),
-    Axis(fig[2, 1]),
-    Axis(fig[2, 2]),
-]
-for species in axes(metacommunity, 3)
-    tl = trophic_level[species]
-    scatter!(axs[tl], vec(environment), vec(metacommunity[:, :, species, end]))
-end
-
-abund = dropdims(mapslices(sum, metacommunity; dims = (1, 2)); dims = (1, 2))
-for species in axes(abund, 1)
-    lines!(axs[end], abund[species, 50:end])
-end
-
-current_figure()
