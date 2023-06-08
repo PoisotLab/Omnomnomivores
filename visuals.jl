ff5d26b5c923509e32ac22f423370ae4d7468664
Tanya Strydom <tanya.strydom@icloud.com>
2023-06-08 14:11:49 +0200

    :sparkles: add richness to dx plot

--- visuals.jl
+++ visuals.jl
@@ -23,12 +23,15 @@ axs = [
     Axis(fig[1, 2],
     xlabel = "Environment value",
     ylabel = "Abundance"),
-    Axis(fig[2, 1],
+    Axis(fig[1, 3],
     xlabel = "Environment value",
     ylabel = "Abundance"),
-    Axis(fig[2, 2],
+    Axis(fig[2, 1:3],
     xlabel = "Generation",
     ylabel = "Abundance"),
+    Axis(fig[3, 1:3],
+    xlabel = "Generation",
+    ylabel = "Species Richness"),
 ]
 for species in axes(metacommunity, 3)
     tl = trophic_level[species]
@@ -38,9 +41,16 @@ end
 
 abund = dropdims(mapslices(sum, metacommunity; dims = (1, 2)); dims = (1, 2))
 for species in axes(abund, 1)
-    lines!(axs[end], abund[species, 1:end], color = species_col[species])
-    ylims!(axs[end], -0.001, 0.01)
+    lines!(axs[4], abund[species, 1:end], color = species_col[species])
+    ylims!(axs[4], -0.001, 0.01)
 end
+
+abund[findall(abund .> 0.0), 1] .= 1.0
+lines!(axs[5], vec(sum(abund[1:40,:], dims = 1)), color = "green", label = "plant")
+lines!(axs[5], vec(sum(abund[41:63,:], dims = 1)), color = "blue", label = "herbivore")
+lines!(axs[5], vec(sum(abund[64:end,:], dims = 1)), color = "red", label = "carnivore")
+axislegend()
 
 current_figure()
 
+save("figures/diagnostics.png", fig)
